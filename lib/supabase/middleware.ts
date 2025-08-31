15fc4fe6df1859b11f4a5c2dbb87b1d2de946184
Johnniel Mar <marjohnniel@gmail.com>
2025-08-31 21:40:43 +0800

    Update middleware.ts

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -17,7 +17,6 @@ export async function updateSession(request: NextRequest) {
           return request.cookies.getAll()
         },
         setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value }) => request.cookies.set(name, value))
           supabaseResponse = NextResponse.next({
             request,
           })
@@ -63,4 +62,5 @@ export async function updateSession(request: NextRequest) {
   // of sync and terminate the user's session prematurely!
 
   return supabaseResponse
+}
 }
